e3d1a51732cd696f0afd3df2d24521b81e78dd0b
katerinasolomakha <katenok7@gmail.com>
2022-10-15 14:11:05 +0300

    Add task5.py

---
+++ task5.py
@@ -0,0 +1,18 @@
+from re import M
+
+
+N=int(input())
+pupils={}
+MARKS_COUNT=3
+marks=[0]*MARKS_COUNT
+
+for i in range(N):
+    pupil=input().split()
+    last_name=pupil[0]
+    pupils[pupil[0]]=[int(i) for i in pupil[1:]]
+    for i in range(MARKS_COUNT):
+        marks[i]+=pupils[last_name][i]
+
+print('-'*20)
+avg_marks=(str('%.2f' % (marks[i]/N)) for i in range(MARKS_COUNT))
+print(' '.join(avg_marks))
