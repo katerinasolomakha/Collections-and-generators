a73f244b4e740d1bae6735091c53a253eeedd224
katerinasolomakha <katenok7@gmail.com>
2022-10-15 13:56:07 +0300

    Add task3.py and task4.py

---
+++ task4.py
@@ -0,0 +1,13 @@
+N=int(input())
+pupils={}
+MARKS_COUNT=3
+output=[]
+for i in range(N):
+    pupil=input().split()
+    last_name=pupil[0]
+    pupils[pupil[0]]=[int(i) for i in pupil[1:]]
+    avg_mark=sum(pupils[pupil[0]])/MARKS_COUNT
+    output.append('%s %.2f' % (last_name, avg_mark))
+
+print('-'*20)
+print('\n'.join(output))
